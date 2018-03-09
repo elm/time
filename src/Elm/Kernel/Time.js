010ba0763de11e158798aa4882976438097b7f4d
Evan Czaplicki <evancz@users.noreply.github.com>
2018-03-09 12:40:02 -0800

    Initial commit

---
+++ src/Elm/Kernel/Time.js
@@ -0,0 +1,23 @@
+/*
+
+import Elm.Kernel.Scheduler exposing (binding, succeed)
+
+*/
+
+
+function _Time_now(toPosix)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		callback(__Scheduler_succeed(toPosix(Date.now())));
+	});
+}
+
+var _Time_setInterval = F2(function(interval, task)
+{
+	return __Scheduler_binding(function(callback)
+	{
+		var id = setInterval(function() { _Scheduler_rawSpawn(task); }, interval);
+		return function() { clearInterval(id); };
+	});
+});
---
+++ src/Elm/Kernel/Time.server.js
@@ -0,0 +1,14 @@
+/*
+
+import Elm.Kernel.Scheduler exposing (binding)
+
+*/
+
+
+function _Time_neverResolve()
+{
+	return __Scheduler_binding(function() {});
+}
+
+var _Time_now = _Time_neverResolve;
+var _Time_setInterval = F2(_Time_neverResolve);
