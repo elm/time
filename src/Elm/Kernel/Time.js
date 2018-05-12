0feeac909ac3ee77b3708972172e1ce3a71c9c53
Evan Czaplicki <evancz@users.noreply.github.com>
2018-05-12 14:00:03 +0200

    Fix the time zone offset
    I assumed that an offset of 60 minutes meant you would be in GMT+1 and
    an offset of -60 would be GMT-1. What a fool I was. Of course. It is
    the opposite.

--- src/Elm/Kernel/Time.js
+++ src/Elm/Kernel/Time.js
@@ -29,7 +29,7 @@ function _Time_here()
 	return __Scheduler_binding(function(callback)
 	{
 		callback(__Scheduler_succeed(
-			A2(__Time_customZone, new Date().getTimezoneOffset(), __List_Nil)
+			A2(__Time_customZone, -(new Date().getTimezoneOffset()), __List_Nil)
 		));
 	});
 }
